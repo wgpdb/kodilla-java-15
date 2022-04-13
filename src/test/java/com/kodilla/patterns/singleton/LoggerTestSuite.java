f81bf43345c04c2bd09cd5d2bb1dfc27874c1e5e
Dariusz <dariusz.borek@gmail.com>
2022-04-13 23:11:34 +0200

    15.1 First commit

---
+++ src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    private Logger logger;
+
+    @Test
+    void testGetLastLog() {
+        //Given
+        logger = Logger.INSTANCE;
+        logger.log("This is a log entry");
+
+        //When
+        String log = logger.getLastLog();
+
+        //Then
+        assertEquals("This is a log entry", log);
+    }
+}
