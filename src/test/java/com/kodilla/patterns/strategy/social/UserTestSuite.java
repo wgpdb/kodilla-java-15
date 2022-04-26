11f06a1ddcca0016b221f1866a7d42159e2b386b
Dariusz <dariusz.borek@gmail.com>
2022-04-26 18:46:29 +0200

    15.3 Added parametrized test

--- src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
+++ src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -1,8 +1,14 @@
 package com.kodilla.patterns.strategy.social;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class UserTestSuite {
 
@@ -44,5 +50,18 @@ public class UserTestSuite {
 
         //Then
         assertEquals("share on twitter [X-generation]", adamWillShareOn);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideUserForSharingStrategy")
+    void sharingStrategyParametrizedTest(User user, String expected) {
+    }
+
+    private static Stream<Arguments> provideUserForSharingStrategy() {
+        return Stream.of(
+                arguments(new Millennial("John"), "share on snapchat [millennial]"),
+                arguments(new YGeneration("Adam"), "share on facebook [Y-generation]"),
+                arguments(new XGeneration("Anna"), "share on twitter [X-generation")
+        );
     }
 }
