af279a11073d2acb8c029bc54f56bfe8e7473381
Dariusz <dariusz.borek@gmail.com>
2022-04-29 10:04:54 +0200

    15.4 Added parametrized test

--- src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
+++ src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -1,8 +1,14 @@
 package com.kodilla.patterns.factory.tasks;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TaskFactoryTestSuite {
 
@@ -46,5 +52,19 @@ public class TaskFactoryTestSuite {
         //Then
         assertTrue(driving.isTaskExecuted());
         assertEquals("refuel", driving.getTaskName());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTask")
+    void taskFactoryParametrizedTest(Task task, String expected) {
+        assertEquals(expected, task.getTaskName());
+    }
+
+    private static Stream<Arguments> provideTask() {
+        return Stream.of(
+                arguments(new TaskFactory().completeTask(TaskFactory.SHOPPING), "go shopping"),
+                arguments(new TaskFactory().completeTask(TaskFactory.PAINTING), "repaint bedroom"),
+                arguments(new TaskFactory().completeTask(TaskFactory.DRIVING), "refuel")
+        );
     }
 }
